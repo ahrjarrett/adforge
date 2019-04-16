5daaafc1778f0d8f787317eb1ebfa4ae209deb45
Andrew Jarrett <ahrjarrett@gmail.com>
2019-04-15 20:57:30 -0500

    fix route

--- src/components/routes/index.tsx
+++ src/components/routes/index.tsx
@@ -1,21 +1,12 @@
 import * as React from 'react'
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import App from '../App'
-import { About } from '../About'
-import { Contact } from '../Contact'
-import Project from '../Project'
-import AddProject from '../AddProject'
 import { Adforge } from "../Adforge"
 
 const AppRouter: React.FC = () => (
   <Router>
-    <Route path="/" exact component={App} />
-    <Route path="/about" exact component={About} />
-    <Route path="/contact" exact component={Contact} />
-    <Route path="/ownlocal/adforge-preview" exact component={Adforge} />
-    <Route path="/project/add" exact component={AddProject} />
-    <Route path="/projects/:id" component={Project} />
+    <Route path="/" exact component={Adforge} />
   </Router>
 )
 
