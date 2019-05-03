ec3ee8ffbaf0db08a24e8561054ca5a110c7477a
Andrew Jarrett <ahrjarrett@gmail.com>
2019-05-03 15:47:08 -0500

    update home route to serve images

--- src/components/routes/index.tsx
+++ src/components/routes/index.tsx
@@ -6,15 +6,17 @@ import { AdforgeSizes } from "../AdforgeSizes"
 
 const AppRouter: React.FC = () => (
   <Router>
-    <Route path="/" exact render={() => (
-      <div style={{textAlign: 'center'}}>
-        <Link to="/images" style={{marginRight: '25px'}}>Calculate image offsets</Link>
-        <Link to="/sizes" style={{marginLeft: '25px'}}>Check widget sizes</Link>
-      </div>
-    )} />
+    <Route path="/" exact component={Adforge} />
     <Route path="/images" exact component={Adforge} />
     <Route path="/sizes" exact component={AdforgeSizes} />
   </Router>
 )
 
 export default AppRouter
+
+//    <Route path="/" exact render={() => (
+//      <div style={{textAlign: 'center'}}>
+//        <Link to="/images" style={{marginRight: '25px'}}>Calculate image offsets</Link>
+//        <Link to="/sizes" style={{marginLeft: '25px'}}>Check widget sizes</Link>
+//      </div>
+//    )} />
